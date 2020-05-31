f7fc6b69b0924879f7c8314e87b3a112fad9df4e
minion0324 <gru.minion.0324@gmail.com>
2020-06-01 00:05:32 +0300

    update some screens to add header bar

---
+++ src/styles/common.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: white;
+`;
+
+const ImageIcon = styled.Image`
+  width: 30px;
+  resizeMode: contain;
+`;
+
+export {
+  Container,
+  ImageIcon,
+};
