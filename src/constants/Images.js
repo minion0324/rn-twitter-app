1a26226ade0f9e4f645de4a34596c575893a4df7
minion0324 <gru.minion.0324@gmail.com>
2020-05-31 23:58:47 +0300

    add some assets

---
+++ src/constants/Images.js
@@ -0,0 +1,10 @@
+//
+const PATH = 'src/assets/images/';
+
+export default {
+  TWITTER_LOGO: require(`${PATH}twitter.png`),
+  ICON_STARS: require(`${PATH}stars.png`),
+  ICON_SETTINGS: require(`${PATH}setting.png`),
+  ERIN: require(`${PATH}erin.png`),
+  AVATAR: require(`${PATH}avatar.png`),
+};
